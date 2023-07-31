1823ba14f31f61177bb6b2931b13c72330fd10ce
Marcel Suter <marcel.suter@bzz.ch>
2023-07-31 14:53:49 +0200

    updated tests

--- main_test.py
+++ main_test.py
@@ -1,37 +1,37 @@
 import main
 
 
-def larger_test(capsys, monkeypatch):
+def test_larger(capsys, monkeypatch):
     inputs = iter([3571, 56872])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main.larger()
     captured = capsys.readouterr()
-    assert captured.out == '56872\n'
+    assert captured.out == '56872 is greater\n'
 
 
-def boolean_test(capsys):
+def test_boolean(capsys):
     main.boolean()
     captured = capsys.readouterr()
     assert captured.out == 'Wahr\n'
 
 
-def modulo_test1(capsys, monkeypatch):
+def test_modulo1(capsys, monkeypatch):
     inputs = iter([17])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main.modulo()
     captured = capsys.readouterr()
-    assert captured.out == 'odd\n'
+    assert captured.out == 'You entered 17 which is an odd integer.\n'
 
 
-def modulo_test2(capsys, monkeypatch):
+def test_modulo2(capsys, monkeypatch):
     inputs = iter([68])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main.modulo()
     captured = capsys.readouterr()
-    assert captured.out == 'even\n'
+    assert captured.out == 'You entered 68 which is an even integer.\n'
 
 
-def nested_test1(capsys, monkeypatch):
+def test_nested1(capsys, monkeypatch):
     inputs = iter([-5])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main.nested()
@@ -39,7 +39,7 @@ def nested_test1(capsys, monkeypatch):
     assert captured.out == 'Less than zero\n'
 
 
-def nested_test2(capsys, monkeypatch):
+def test_nested2(capsys, monkeypatch):
     inputs = iter([197])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main.nested()
@@ -47,9 +47,10 @@ def nested_test2(capsys, monkeypatch):
     assert captured.out == 'Greater than 100\n'
 
 
-def nested_test3(capsys, monkeypatch):
+def test_nested3(capsys, monkeypatch):
     inputs = iter([63])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main.nested()
     captured = capsys.readouterr()
     assert captured.out == 'Between 0 and 100\n'
+
