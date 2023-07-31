ac45abc4b6c0c0d42fa08fb3908d2620115d13cf
Marcel Suter <marcel.suter@bzz.ch>
2023-07-31 14:53:15 +0200

    initial commit

--- main.py
+++ main.py
@@ -2,32 +2,19 @@ def larger():
     num1 = int(input("Please enter an integer:\n"))
     num2 = int(input("Please enter an integer:\n"))
 
-    # TODO: Please print the bigger number, only use one ternary-operator to do so
+    print(f'{num1} is greater') if (num1 > num2) else print(f'{num2} is greater')
 
 
 def boolean():
     value = True
 
-    # problem:
-    print(value)  # 'True' and 'False' will be printed instead of 'Wahr' and 'Falsch'
-
-    # With if-else-statement
-    if value:
-        print('Wahr')
-    else:
-        print('Falsch')
-
-    # TODO: Please print the boolean-value in german, only use one ternary-operator to do so
+    print('Wahr' if value else 'Falsch')
 
 
 def modulo():
     x = int(input("Please enter an integer:\n"))
 
-    # TODO: replace the if..else with a ternary-operator
-    if x % 2 == 0:
-        int_type = 'even'
-    else:
-        int_type = 'odd'
+    int_type = 'odd' if x % 2 else 'even'
 
     print(f'You entered {x} which is an {int_type} integer.')
 
@@ -35,13 +22,7 @@ def modulo():
 def nested():
     number = int(input('Enter number between -100 and +200'))
 
-    if (number < 0):
-        print('Less than zero')
-    else:
-        if number >= 0 and number <= 100:
-            print('Between 0 and 100')
-        else:
-            print('Greater than 100')
+    print('Less than zero' if number < 0 else 'Between 0 and 100' if number >= 0 and number <= 100 else 'Greater than 100')
 
 
 if __name__ == '__main__':
