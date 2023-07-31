e732a6d1f42045b587ec524b57b0d3ec703cdf4c
Marcel Suter <marcel.suter@bzz.ch>
2023-07-31 14:44:50 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def larger():
+    num1 = int(input("Please enter an integer:\n"))
+    num2 = int(input("Please enter an integer:\n"))
+
+    # TODO: Please print the bigger number, only use one ternary-operator to do so
+
+
+def boolean():
+    value = True
+
+    # problem:
+    print(value)  # 'True' and 'False' will be printed instead of 'Wahr' and 'Falsch'
+
+    # With if-else-statement
+    if value:
+        print('Wahr')
+    else:
+        print('Falsch')
+
+    # TODO: Please print the boolean-value in german, only use one ternary-operator to do so
+
+
+def modulo():
+    x = int(input("Please enter an integer:\n"))
+
+    # TODO: replace the if..else with a ternary-operator
+    if x % 2 == 0:
+        int_type = 'even'
+    else:
+        int_type = 'odd'
+
+    print(f'You entered {x} which is an {int_type} integer.')
+
+
+def nested():
+    number = int(input('Enter number between -100 and +200'))
+
+    if (number < 0):
+        print('Less than zero')
+    else:
+        if number >= 0 and number <= 100:
+            print('Between 0 and 100')
+        else:
+            print('Greater than 100')
 
 
 if __name__ == '__main__':
-    calculate()
+    larger()
+    boolean()
+    modulo()
+    nested()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,55 @@
 import main
 
-def test(capsys):
-    main.calculate()
+
+def larger_test(capsys, monkeypatch):
+    inputs = iter([3571, 56872])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.larger()
     captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+    assert captured.out == '56872\n'
+
+
+def boolean_test(capsys):
+    main.boolean()
+    captured = capsys.readouterr()
+    assert captured.out == 'Wahr\n'
+
+
+def modulo_test1(capsys, monkeypatch):
+    inputs = iter([17])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.modulo()
+    captured = capsys.readouterr()
+    assert captured.out == 'odd\n'
+
+
+def modulo_test2(capsys, monkeypatch):
+    inputs = iter([68])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.modulo()
+    captured = capsys.readouterr()
+    assert captured.out == 'even\n'
+
+
+def nested_test1(capsys, monkeypatch):
+    inputs = iter([-5])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.nested()
+    captured = capsys.readouterr()
+    assert captured.out == 'Less than zero\n'
+
+
+def nested_test2(capsys, monkeypatch):
+    inputs = iter([197])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.nested()
+    captured = capsys.readouterr()
+    assert captured.out == 'Greater than 100\n'
+
+
+def nested_test3(capsys, monkeypatch):
+    inputs = iter([63])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.nested()
+    captured = capsys.readouterr()
+    assert captured.out == 'Between 0 and 100\n'
